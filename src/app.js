fc1779f8b9ced3a460d385c6d9609b887947b0f9
Harish10092004 <harishemkcecsbs2022@gmail.com>
2025-04-13 15:33:06 +0530

    file modified

--- src/app.js
+++ src/app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const todoRouter =require('./routes/todo'); 
 const app = express();
 const port = 3000;
-
+app.use(express.json());
 app.use('/todo',todoRouter);
 
 app.listen(port, ()=>{
--- src/routes/todo.js
+++ src/routes/todo.js
@@ -1,4 +1,13 @@
 const express = require('express');
+const { createTODO } = require('../models/todo');
 const router = express.Router();
+
+
+router.post('/',async(req,res)=>{
+  const {title,description}=req.body;
+  const todo = await createTODO(title,description);
+  // console.log({body:req.body});
+  res.send(todo);
+});
 
 module.exports= router;
