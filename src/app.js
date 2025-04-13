2e4bc4164779ad57c1607852a3ff0804d6432990
Harish10092004 <harishemkcecsbs2022@gmail.com>
2025-04-13 14:57:57 +0530

    file added

---
+++ src/app.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const todoRouter =require('./routes/todo'); 
+const app = express();
+const port = 3000;
+
+app.use('/todo',todoRouter);
+
+app.listen(port, ()=>{
+  console.log(`server is running on http://localhost:${port}`);
+});
